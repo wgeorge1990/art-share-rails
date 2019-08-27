01313c18ba0878bb828c5d34db2e001fed4d2e51
wgeorge1990 <williameverettgeorge@icloud.com>
2019-08-27 10:26:21 -0400

    commented on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    ##Need to add validations for password and email.
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
     has_many :photos
